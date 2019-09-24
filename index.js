8b4e692b4cc9d12ba0af56fb8d71eba1c572641c
2ndyrGroupC <2ndyrGroupC@pn.local>
2019-09-24 19:34:48 +0800

    update files in the afternoon 7:34 sa 24-2019

--- index.js
+++ index.js
@@ -2,22 +2,33 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
+var express = require('express');
+var users = []
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
+io.on('connection', function (socket) {
+  socket.on('chat message', function (msg) {
     io.emit('chat message', msg);
   });
-  
+
   socket.on('typing', function (typing) {
     //io.emit('typing');
     socket.broadcast.emit('typing', typing)
-    })
-});
 
-http.listen(port, function(){
+  })
+
+  socket.on('online', function (names) {
+    users.push(names);
+    socket.emit("onlineUsers",users)
+    console.log(users)
+
+  });
+});
+app.use(express.static('public'));
+
+http.listen(port, function () {
   console.log('listening on *:' + port);
 });
